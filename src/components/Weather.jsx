6bf0233723fb648eae7eced630bd572ad56b8990
Yash <yash9570raj@gmail.com>
2024-08-11 20:55:18 +0530

    first commit

---
+++ src/components/Weather.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import "./Weather.css";
+import search_icon from "../assets/search.png";
+import clear_icon from "../assets/clear.png";
+import cloud_icon from "../assets/cloud.png";
+import drizzle_icon from "../assets/drizzle.png";
+import humidity_icon from "../assets/humidity.png";
+import rain_icon from "../assets/rain.png";
+import snow_icon from "../assets/snow.png";
+import wind_icon from "../assets/wind.png";
+
+const Weather = () => {
+  return (
+    <div className="weather">
+      <div className="search-bar">
+        <input type="text" placeholder="Search" />
+        <img src={search_icon} alt="" />
+      </div>
+      <img src={clear_icon} alt="" className="weather-icon" />
+      <p className="temperature">20°C</p>
+      <p className="location">Bihar</p>
+      <div className="weather-data">
+        <div className="col"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Weather;
